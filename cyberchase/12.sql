eb233536bd125dbeb5dae41cdfecefc4024d0c27
codeinblue1 <limardo50@icloud.com>
2026-07-20 18:27:35 -0700

    Delete cyberchase directory

--- cyberchase/12.sql
+++
@@ -1,3 +0,0 @@
---In 12.sql, count the number of unique episode titles.
-SELECT COUNT(DISTINCT title)
-FROM episodes;
--- cyberchase/13.sql
+++
@@ -1,6 +0,0 @@
---In 13.sql, write a SQL query to explore a question of your choice.
---This query should: Involve at least one condition, using WHERE with AND or OR
-SELECT id, title, topic, season
-FROM episodes
-WHERE topic IS NOT NULL
-AND season BETWEEN 5 AND 8;
